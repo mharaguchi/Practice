2fbbaf4da3a46fdd74cba948a5ff92dcc416a994
mharaguchi <4097799+mharaguchi@users.noreply.github.com>
2024-04-25 10:00:33 -0600

    LeetCode 226

--- PracticeProblems/PracticeProblems/Grind75.cs
+++ PracticeProblems/PracticeProblems/Grind75.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.Metrics;
+using System.Security.AccessControl;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -131,6 +132,42 @@ namespace PracticeProblems
             return true;
         }
 
-
+        /* 226 - Invert Binary Tree */
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return root;
+            }
+            if (root.left == null && root.right == null)
+            {
+                return root;
+            }
+            else if (root.left == null && root.right != null)
+            {
+                root.left = root.right;
+                root.right = null;
+            }
+            else if (root.right == null && root.left != null)
+            {
+                root.right = root.left;
+                root.left = null;
+            }
+            else
+            {
+                TreeNode temp = root.left;
+                root.left = root.right;
+                root.right = temp;
+            }
+            if (root.left != null)
+            {
+                InvertTree(root.left);
+            }
+            if (root.right != null)
+            {
+                InvertTree(root.right);
+            }
+            return root;
+        }
     }
 }
--- PracticeProblems/PracticeProblems/SharedClasses.cs
+++ PracticeProblems/PracticeProblems/SharedClasses.cs
@@ -16,4 +16,17 @@ namespace PracticeProblems
             this.next = next;
         }
     }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }

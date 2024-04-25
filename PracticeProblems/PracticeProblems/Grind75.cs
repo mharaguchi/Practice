e09b6122bc16d7b7c78115c3f53c361b4ec200d9
mharaguchi <4097799+mharaguchi@users.noreply.github.com>
2024-04-25 09:44:26 -0600

    LeetCode 121 & 125

--- PracticeProblems/PracticeProblems/Grind75.cs
+++ PracticeProblems/PracticeProblems/Grind75.cs
@@ -1,15 +1,11 @@
 ﻿using System.Diagnostics.Metrics;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace PracticeProblems
 {
     internal class GrindProblems
     {
-
-        public void Run()
-        {
-
-        }
-
         /* 21 - Merge Two Sorted Lists */
         public ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
@@ -71,5 +67,70 @@ namespace PracticeProblems
 
             return preHead.next;
         }
+
+
+        /* 121 - Best Time To Buy and Sell Stock */
+        public int MaxProfit(int[] prices)
+        {
+            var curMin = 0;
+            var curMaxDiff = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                var thisVal = prices[i];
+                if (i == 0)
+                {
+                    curMin = thisVal;
+                }
+                var diff = thisVal - curMin;
+                if (diff > curMaxDiff && diff > 0)
+                {
+                    curMaxDiff = diff;
+                }
+                if (thisVal < curMin)
+                {
+                    curMin = thisVal;
+                }
+            }
+
+            return curMaxDiff;
+        }
+
+        /* 125 - Valid Palindrome */
+        public bool IsPalindrome(string s)
+        {
+            var lowerString = s.ToLower();
+            var cleanStringBuilder = new StringBuilder();
+
+            for(int i = 0; i < lowerString.Length; i++)
+            {
+                if ((lowerString[i] >= 48 && lowerString[i] <= 57) || (lowerString[i] >= 97 && lowerString[i] <= 122))
+                {
+                    cleanStringBuilder.Append(lowerString[i]);
+                }
+            }
+
+            var cleanString = cleanStringBuilder.ToString();
+
+            if (cleanString.Length == 0)
+            {
+                return true;
+            }
+
+            var head = 0;
+            var tail = cleanString.Length - 1;
+            while (head <= tail)
+            {
+                if (cleanString[head] != cleanString[tail])
+                {
+                    return false;
+                }
+                head++;
+                tail--;
+            }
+
+            return true;
+        }
+
+
     }
 }

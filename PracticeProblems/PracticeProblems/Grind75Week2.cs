58ce32671f9dd33e01811920c69d09878afe9311
mharaguchi <4097799+mharaguchi@users.noreply.github.com>
2024-05-01 08:02:01 -0600

    LeetCode 278

---
+++ PracticeProblems/PracticeProblems/Grind75Week2.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.Metrics;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PracticeProblems
+{
+    internal class Grind75Week2
+    {
+        /* 278 - First Bad Version */
+        public int FirstBadVersion(int n)
+        {
+            var left = 1;
+            var right = n;
+
+            while (left < right)
+            {
+                var mid = left + (right - left) / 2;
+                if (IsBadVersion(mid))
+                {
+                    right = mid;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+
+        public bool IsBadVersion(int n)
+        {
+            return true;
+        }
+    }
+}
